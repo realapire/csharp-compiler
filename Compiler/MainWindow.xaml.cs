3d0b75601038ed45af8bc44710c288c4d51f07d9
apire <duboisandre092@gmail.com>
2022-10-06 04:40:48 +0200

    csharp compiler

---
+++ Compiler/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.CSharp;
+using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+
+namespace Compiler
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void compileButton_Click(object sender, RoutedEventArgs e)
+        {
+            resultsOutput.Text = String.Empty;
+            CSharpCodeProvider csc = new CSharpCodeProvider(new Dictionary<string, string>() { { "CompilerVersion", frameworkInput.Text } });
+            CompilerParameters parameters = new CompilerParameters(new[] { "mscorlib.dll", "System.Core.dll" }, outputInput.Text, true);
+            parameters.GenerateExecutable = true;
+            CompilerResults results = csc.CompileAssemblyFromSource(parameters, codeInput.Text);
+            if(results.Errors.HasErrors)
+            {
+                results.Errors.Cast<CompilerError>().ToList().ForEach(error => resultsOutput.Text += error.ErrorText + "\r\n");
+            } else
+            {
+                resultsOutput.Text = "---Build succeeded---";
+                Process.Start(System.AppDomain.CurrentDomain.BaseDirectory + "/" + outputInput.Text);
+            }
+        }
+    }
+}
